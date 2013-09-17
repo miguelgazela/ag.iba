31378ba83c9042a4a687b9b1a13631cf2f7a8bd3
miguelgazela <miguel.gazela@gmail.com>
2013-09-17 23:34:33 +0100

    starting engines

---
+++ database/schema.sql
@@ -0,0 +1,40 @@
+drop table if exists utilizadores;
+drop table if exists clientes;
+drop table if exists impostos;
+drop table if exists actividades;
+
+create table utilizadores (
+	id_utilizador integer primary key autoincrement,
+	username text,
+	pass_hash text,
+	tipo integer
+);
+
+create table clientes (
+	id_cliente integer primary key autoincrement,
+	nome text,
+	morada text,
+	cidade text,
+	freguesia text,
+	cod_postal text,
+	nif text
+);
+
+create table impostos (
+	id_imposto integer primary key autoincrement,
+	id_cliente not null references clientes(id_cliente),
+	marca text,
+	modelo text,
+	matricula text,
+	data_matricula date,
+	data_pagamento date,
+);
+
+create table actividades (
+	id_actividade integer primary key autoincrement,
+	id_utilizador not null references utilizadores(id_utilizador),
+	id_cliente references clientes(id_cliente),
+	id_imposto references impostos(id_imposto),
+	tipo integer,
+	data_alteracao date
+);
