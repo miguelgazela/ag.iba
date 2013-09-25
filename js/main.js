387be550bb29f3aba5c1acc48634987255ad0146
miguelgazela <miguel.gazela@gmail.com>
2013-09-25 15:56:15 +0100

    a bit of more bytes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     console.log("JQuery activated");
 
+    $('#dp2').datepicker('show');
+
     $('input[type="checkbox"').click(function(e) {
         console.log("Clicked a checkbox");
     });
