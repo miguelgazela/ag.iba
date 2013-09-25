9b43915978567ef3320419df1be8f63ad4edcc8f
miguelgazela <miguel.gazela@gmail.com>
2013-09-25 21:44:53 +0100

    are we there yet?

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
     console.log("JQuery activated");
 
-    $('#dp2').datepicker('show');
+    // handlers for datepickers
+    $('#input_plate_date').focus(function() {
+        $('#input_plate_date').datepicker('show');
+    });
+    $('#input_due_date').focus(function() {
+        $('#input_due_date').datepicker('show');
+    });
+
 
     $('input[type="checkbox"').click(function(e) {
         console.log("Clicked a checkbox");
