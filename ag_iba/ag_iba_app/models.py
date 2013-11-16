d304cefc8801814750cda62fa50b4b6d4c198b13
miguelgazela <miguel.gazela@gmail.com>
2013-11-16 12:51:04 +0000

    Revert af65fd1..de84203
    This rolls back to commit af65fd1e211135bb816258924fb1348ed8d9f552.

--- ag_iba/ag_iba_app/models.py
+++ ag_iba/ag_iba_app/models.py
@@ -1,25 +1,3 @@
 from django.db import models
 
-class User(models.Model):
-
-    USER_PERMISSIONS = (
-        ('ST', 'Standard'),
-        ('AD', 'Admin'),
-    )
-
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    permission = models.CharField(max_length=2, choices=USER_PERMISSIONS)
-    date_joined = models.DateTimeField()
-    activated = models.BooleanField(default=False)
-
-    def __unicode__(self):
-        return "{first} {last}".format(
-            first=self.first_name,
-            last=self.last_name
-        )
-
-    def is_active(self):
-        return self.activated
-
-
+# Create your models here.
