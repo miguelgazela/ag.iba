dcdff7cdd04479bbe0803945dddf0fc9f17c4fcf
miguelgazela <miguel.gazela@gmail.com>
2014-01-18 17:16:21 +0000

    added models for clients and taxes

--- ag_iba/app/admin.py
+++ ag_iba/app/admin.py
@@ -1,1 +1,12 @@
 from django.contrib import admin
+from app.models import Client
+from app.models import Tax
+
+class ClientAdmin(admin.ModelAdmin):
+    pass
+
+class TaxAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Tax, TaxAdmin)
--- ag_iba/app/models.py
+++ ag_iba/app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Client(models.Model):
+    name = models.CharField(max_length=160)
+    adress = models.CharField(max_length=160)
+    local = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    postal = models.CharField(max_length=8)
+    nif = models.CharField(max_length=9)
+    added = models.DateTimeField(auto_now_add=True)
+
+class Tax(models.Model):
+    client = models.ForeignKey(Client)
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    plate = models.CharField(max_length=8)
+    plate_date = models.DateField()
+
